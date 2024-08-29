6cbd212ec9913a0eb3538a5563ac26e93476b828
Jannik Taubert <git@tbrt.eu>
2024-08-29 12:09:02 +0200

    initial commit

---
+++ flatastic.js
@@ -0,0 +1,12 @@
+﻿module.exports = function (RED) {
+    function Flatastic() {
+        RED.nodes.createNode(this, config);
+    }
+    RED.nodes.registerType("flatastic", Flatastic, {
+        credentials: {
+            username: {type: 'text'},
+            password: {type: 'password'},
+            apiKey: {type: 'password'},
+        }
+    });
+}
---
+++ send-shout.js
@@ -0,0 +1,12 @@
+﻿module.exports = function(RED) {
+    function SendShout(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function (msg) {
+            msg.payload = "Hello World!";
+            node.send(msg);
+        });
+
+        RED.nodes.registerType("send shout", SendShout);
+    }
+}
